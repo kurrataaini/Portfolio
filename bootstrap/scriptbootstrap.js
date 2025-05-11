b61dd044ff8a9be8dd978b46e930f65091b6cf64
kurrataaini <kurrataaini2002@gmail.com>
2025-05-11 19:38:54 +0700

    masuk

---
+++ bootstrap/scriptbootstrap.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+  
+    if (!name || !email || !message) {
+      alert("Please fill in all fields.");
+      return false;
+    }
+  
+    alert("Message sent! (simulation only)");
+    return true;
+  }
+  
